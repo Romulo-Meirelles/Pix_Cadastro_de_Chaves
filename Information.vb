689c106046161421ccfff6b9a0805629587dfdbd
Rômulo Meirelles <52598055+Romulo-Meirelles@users.noreply.github.com>
2021-04-29 04:58:12 -0300

    Add files via upload

---
+++ Information.vb
@@ -0,0 +1,122 @@
+﻿Imports MessagingToolkit.QRCode
+Imports MessagingToolkit
+
+Public Class Information
+
+    Private Sub Information_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Label5.Text = My.Application.Info.Version.ToString
+    End Sub
+
+    Private Sub Information_MouseClick(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseClick
+        Me.Close()
+    End Sub
+
+    Private Sub Label2_Click(sender As System.Object, e As System.EventArgs) Handles Label2.Click
+        Try
+            Process.Start("https://github.com/Romulo-Meirelles")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Label4_Click(sender As System.Object, e As System.EventArgs) Handles Label4.Click
+        Try
+            Process.Start("https://github.com/Romulo-Meirelles/Pix_Cadastro_de_Chaves")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Timer_Mensagem_Tick(sender As System.Object, e As System.EventArgs) Handles Timer_Mensagem.Tick
+        Mensagem_lbl.Visible = False
+        Timer_Mensagem.Stop()
+    End Sub
+    Private Sub Mensagem(ByVal MSG As String)
+        Mensagem_lbl.Text = MSG
+        Mensagem_lbl.Visible = True
+        Timer_Mensagem.Start()
+    End Sub
+    Private Sub Label6_Click(sender As System.Object, e As System.EventArgs) Handles Label6.Click
+        Try
+            Dim BTC As String = "1BZpHRGAePYE8J4sSq6v9QkBPAeVrTffgt"
+            Dim qrencod As New QRCode.Codec.QRCodeEncoder
+            Dim qrcode As Bitmap = qrencod.Encode(BTC)
+            QR_Code_Pic.Image = TryCast(qrcode, Image)
+            QR_Moeda_Nome_lbl.Visible = True
+            QR_Moeda_Nome_lbl.Text = "BitCoin"
+            Clipboard.SetText(BTC)
+            Mensagem("Carteira BTC Copiada!")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+   
+
+    Private Sub Label7_Click(sender As System.Object, e As System.EventArgs) Handles Label7.Click
+        Try
+            Dim BTC As String = "LLkEawSYhM76B1zqHLmZTWbQHjksrwjeWx"
+            Dim qrencod As New QRCode.Codec.QRCodeEncoder
+            Dim qrcode As Bitmap = qrencod.Encode(BTC)
+            QR_Code_Pic.Image = TryCast(qrcode, Image)
+            QR_Moeda_Nome_lbl.Visible = True
+            QR_Moeda_Nome_lbl.Text = "LiteCoin"
+
+            Clipboard.SetText(BTC)
+            Mensagem("Carteira LTC Copiada!")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Label8_Click(sender As System.Object, e As System.EventArgs) Handles Label8.Click
+        Try
+            Dim BTC As String = "0x4454b8f08cc9d147afd93b72bf7066f10f3c2b90"
+            Dim qrencod As New QRCode.Codec.QRCodeEncoder
+            Dim qrcode As Bitmap = qrencod.Encode(BTC)
+            QR_Code_Pic.Image = TryCast(qrcode, Image)
+            QR_Moeda_Nome_lbl.Visible = True
+            QR_Moeda_Nome_lbl.Text = "Ethereum"
+
+            Clipboard.SetText(BTC)
+            Mensagem("Carteira ETH Copiada!")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Label9_Click(sender As System.Object, e As System.EventArgs) Handles Label9.Click
+        Try
+            Dim BTC As String = "D8dJGkba3wL8wuNLDAm7gFSu1KYSVLHxLp"
+            Dim qrencod As New QRCode.Codec.QRCodeEncoder
+            Dim qrcode As Bitmap = qrencod.Encode(BTC)
+            QR_Code_Pic.Image = TryCast(qrcode, Image)
+            QR_Moeda_Nome_lbl.Visible = True
+            QR_Moeda_Nome_lbl.Text = "DogeCoin"
+
+            Clipboard.SetText(BTC)
+            Mensagem("Carteira DOGE Copiada!")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Label10_Click(sender As System.Object, e As System.EventArgs) Handles Label10.Click
+        Try
+            Dim BTC As String = "767938fe-66cd-4831-9b3c-bc9760e9c835"
+            Dim qrencod As New QRCode.Codec.QRCodeEncoder
+            Dim qrcode As Bitmap = qrencod.Encode(BTC)
+            QR_Code_Pic.Image = TryCast(qrcode, Image)
+            QR_Moeda_Nome_lbl.Visible = True
+            QR_Moeda_Nome_lbl.Text = "Pix NuBank"
+
+            Clipboard.SetText(BTC)
+            Mensagem("Carteira PIX Copiada!")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Close_Pic_Click(sender As System.Object, e As System.EventArgs) Handles Close_Pic.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ Pix.vb
@@ -0,0 +1,152 @@
+﻿Imports MessagingToolkit
+Imports MessagingToolkit.QRCode
+
+
+
+Public Class Pix
+
+    Private Sub Pix_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        CarregaBordas(Me)
+        CarregaBordas(Adicionar_btn)
+        CarregaBordas(Remover_btn)
+        Version_lbl.Text = "Versão: " & My.Application.Info.Version.ToString
+        CreatorTable()
+        LoadPix(ListView1)
+    End Sub
+    Private Sub Minimize_Pic_Click(sender As System.Object, e As System.EventArgs) Handles Minimize_Pic.Click
+        Try
+            Me.Hide()
+            NotifyIcon1.BalloonTipText = "Pix Minimizado..."
+            NotifyIcon1.ShowBalloonTip(3000)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Close_Pic_Click(sender As System.Object, e As System.EventArgs) Handles Close_Pic.Click
+        Try
+            End
+            Application.Exit()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub NotifyIcon1_MouseClick(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles NotifyIcon1.MouseClick
+        Try
+            Me.Show()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Try
+            Me.Show()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Pix_Icon_Pic_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Pix_Icon_Pic.MouseDown
+        Form_Move.MouseDown(sender, e)
+    End Sub
+    Private Sub Pix_Icon_Pic_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Pix_Icon_Pic.MouseMove
+        Form_Move.MouseMove(sender, e)
+    End Sub
+    Private Sub Title_Label_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Title_Label.MouseDown
+        Form_Move.MouseDown(sender, e)
+    End Sub
+
+    Private Sub Title_Label_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Title_Label.MouseMove
+        Form_Move.MouseMove(sender, e)
+    End Sub
+
+    Private Sub Superior_Panel_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Superior_Panel.MouseDown
+        Form_Move.MouseDown(sender, e)
+    End Sub
+    Private Sub Superior_Panel_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Superior_Panel.MouseMove
+        Form_Move.MouseMove(sender, e)
+    End Sub
+
+
+    Private Sub Adicionar_btn_Click(sender As System.Object, e As System.EventArgs) Handles Adicionar_btn.Click
+        Try
+            Dim NovaChave As New Adicionar
+            Dim Result As DialogResult = NovaChave.ShowDialog()
+
+            If Result = Windows.Forms.DialogResult.OK Then
+                Mensagem("Chave Pix adicionada com sucesso!")
+            End If
+
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Remover_btn_Click(sender As System.Object, e As System.EventArgs) Handles Remover_btn.Click
+        Try
+
+            If MessageBox.Show("Tem certeza que deseja excluír " & ListView1.Items(ListView1.FocusedItem.Index).SubItems(1).Text & "", "Exclusão.", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = Windows.Forms.DialogResult.No Then
+                Exit Sub
+            End If
+
+            Delete(ListView1.Items(ListView1.FocusedItem.Index).SubItems(0).Text)
+            ListView1.Items.Clear()
+            LoadPix(ListView1)
+            Mensagem("Chave Pix excluída com sucesso!")
+            QR_Code_Pix_Pic.Visible = False
+            QR_Code_lbl.Visible = False
+            QR_Code_Banco_lbl.Visible = False
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Mensagem Exclusão", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End Try
+    End Sub
+
+    Private Sub Timer_Mensagem_Tick(sender As System.Object, e As System.EventArgs) Handles Timer_Mensagem.Tick
+        Try
+            Mensagem_lbl.ForeColor = Color.Red
+            Mensagem_lbl.Text = "Dê 2 cliques para copiar a chave."
+            Timer_Mensagem.Stop()
+        Catch ex As Exception
+        End Try
+    End Sub
+    Private Sub Mensagem(ByVal MSG As String)
+        Mensagem_lbl.ForeColor = Color.LimeGreen
+        Mensagem_lbl.Text = MSG
+        Timer_Mensagem.Start()
+    End Sub
+
+    Private Sub ListView1_MouseDoubleClick(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles ListView1.MouseDoubleClick
+        Try
+            Clipboard.SetText(ListView1.Items(ListView1.FocusedItem.Index).SubItems(2).Text)
+            Mensagem("Chave Pix copiada com sucesso!")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListView1.SelectedIndexChanged
+        Try
+            Dim Pix As String = ListView1.Items(ListView1.FocusedItem.Index).SubItems(2).Text
+            Dim qrencod As New QRCode.Codec.QRCodeEncoder
+            Dim qrcode As Bitmap = qrencod.Encode(Pix)
+            QR_Code_Pix_Pic.Image = TryCast(qrcode, Image)
+            QR_Code_Banco_lbl.Text = ListView1.Items(ListView1.FocusedItem.Index).SubItems(1).Text
+            QR_Code_Pix_Pic.Visible = True
+            QR_Code_lbl.Visible = True
+            QR_Code_Banco_lbl.Visible = True
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Information_Pic_Click(sender As System.Object, e As System.EventArgs) Handles Information_Pic.Click
+        Try
+            Dim Inf As New Information
+            Inf.ShowDialog()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
